11d847e11bd1a68aae662228bcf3bed023d7715e
skubi6 <skubi@skubi.net>
2025-07-06 15:54:55 +0200

    doc

--- wykonanie.sh
+++ wykonanie.sh
@@ -15,3 +15,32 @@ Nie ma restrykcji ze względu na typ obwodu
 Nie ma restrykcji ze względu na liczbę ludnosci gminy
 
 EOF
+
+python lincyferki-merge.py
+
+cat > /dev/null <<EOF
+
+Neeaktualny. Miał złączać wynik lincyferki-prepare.py z
+commission_combined.xlsx
+czyli ze składami komisji po komitetach wyborczych.
+
+Obecnie zbędne, bo lincyferki-prepare.py robi to złączenie
+EOF
+
+python lincyferki-display.py
+
+cat > /dev/null <<EOF
+
+Zasadniczo zmieniam logikę:
+nazwa pliku we jet pierwszym argumentem
+
+-H: histogramy główne
+-c <arg>: cyferki dla kasy <arg> <arg> może być all lub red lub blue lub black
+-o: generuj tabelę outliers, której nazwa, to outliers-<nazwa pliku we>
+-l <int>: z podziałem na ludność gminy ponizej i powyżej <int>. Raczej nie używamy, bo podział według ludności robiony jest wcześniej, w lincyferki-prepare.py
+
+-W: oddizelnie wyliczaj Warszawę (obecnie niezalecane)
+-w: podział na województwa (obecnie niezalecane)
+-m: nie wiem, co to
+-y <rok>: rok. W domysle 2025. Dopuszczalne wartości: 2020, 2025
+EOF

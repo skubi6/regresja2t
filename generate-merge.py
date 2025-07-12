67cc2f8acad33a04fbb5a064826302d6587717dd
skubi6 <skubi@skubi.net>
2025-07-12 02:13:21 +0200

    wygenerowanie pism do prokuratur

--- generate-merge.py
+++ generate-merge.py
@@ -21,27 +21,27 @@ fileGroups = [
     {'desc': 'w miastach ponad 250 tys. mieszkańców',
      'regrNm': 'YmiastoDst250+ar2025.xlsx',
      'outliersNm': 'outliers-YmiastoDst250+ar2025.xlsx',
-     kat:'250+'},
+     'kat':'250+'},
     {'desc': 'w miastach od 100 tys. do 250 tys. mieszkańców',
      'regrNm': 'YmiastoDst100-250+ar2025.xlsx',
      'outliersNm': 'outliers-YmiastoDst100-250+ar2025.xlsx',
-     kat:'100+'},
+     'kat':'100+'},
     {'desc': 'w miastach od 40 tys. do 100 tys. mieszkańców',
      'regrNm': 'YmiastoDst40-100ar2025.xlsx',
      'outliersNm': 'outliers-YmiastoDst40-100ar2025.xlsx',
-     kat:'40+'},
+     'kat':'40+'},
     {'desc': 'w miastach od 20 tys. do 40 tys. mieszkańców',
      'regrNm': 'YmiastoDst20-40ar2025.xlsx',
      'outliersNm': 'outliers-YmiastoDst20-40ar2025.xlsx',
-     kat:'20+'},
+     'kat':'20+'},
     {'desc': 'w miastach poniżej 20 tys. mieszkańców',
      'regrNm': 'YmiastoDst20-ar2025.xlsx',
      'outliersNm': 'outliers-YmiastoDst20-ar2025.xlsx',
-     kat:'20-'},
+     'kat':'20-'},
     {'desc': 'obwoty wiejskie i miejsco-wiejskie',
      'regrNm': 'YwiesDstar2025.xlsx',
      'outliersNm': 'outliers-YwiesDstar2025.xlsx',
-     kat:'wieś'},
+     'kat':'wieś'},
 ]
 
 prokuraturyLista = pd.read_excel('prokuratury-lista.xlsx')
@@ -65,10 +65,10 @@ out['dir'] = prokuraturyLista["Prokuratura Okręgowa"].str.replace(" ", "-", reg
 out['adres'] = out['nazwa']+r'\\'+prokuraturyLista["Adres"].str.replace(", ", r"\\", regex=False)
 
 for g in fileGroups:
-    g += pd.read_excel(
+    g |= pd.read_excel(
         g['outliersNm'],
         sheet_name=["x_edgePOS", "x_edgeNEG"])
-    g['Coefficients'] = pd.read_excel(l['regrNm'], sheet_name='Coefficients')
+    g['Coefficients'] = pd.read_excel(g['regrNm'], sheet_name='Coefficients')
     
 #for l in fileGroups:
 #    print (l)
@@ -80,7 +80,7 @@ EXPLAIN = {'P' : 'niemożliwym',
            'N' : 'nieprawdopodobnym'}
 BENEF = {'POS' : "Karola NAWROCKIEGO", 'NEG' : "Rafała Trzaskowskiego"}
 
-colsToKeep = ["Teryt Gminy", "Gmina", "Powiat", "Nr Komisji", "NAWROCKI Karol Tadeusz", "fitsNAWROCKI Karol Tadeusz",
+colsToKeep = ["Teryt Gminy", "Gmina", "Powiat", "Nr komisji", "NAWROCKI Karol Tadeusz", "fitsNAWROCKI Karol Tadeusz",
               "TRZASKOWSKI Rafał Kazimierz", "fitsTRZASKOWSKI Rafał Kazimierz", "obs_diff", "fit_diff", "D", "diff_std", "x_edge"]
 
 def euro_fmt(x):
@@ -92,21 +92,19 @@ def euro_fmt(x):
     """
     return f"{x:.6f}".replace(".", ",")
 
-formatters = {c: euro_fmt for c in cols_to_keep if pd.api.types.is_float_dtype(df[c])}
-
-for v in out:
+for i, v in out.iterrows():
     (s / v["dir"]).mkdir (parents=True, exist_ok=True)
-    #extracts = {'POS': {l : {} for v, l in THRESHOLDS},
-    #            'NEG': {l : {} for v, l in THRESHOLDS}}
+    #extracts = {'POS': {l : {} for vv, l in THRESHOLDS},
+    #            'NEG': {l : {} for vv, l in THRESHOLDS}}
     extracts = {'POS': {},'NEG': {}}
     for direction in ['POS', 'NEG']:
         probaMax = 2
         for probaMin, label in THRESHOLDS:
             for g in fileGroups:
-                ou = g[x_edge+direction]
-                ext = ou[(ou['x_adge'] >= probaMin) &(ou['x_adge'] < probaMax) & (ou['prokuratura']==v['nazwa'])]
+                ou = g['x_edge'+direction]
+                ext = (ou[(ou['x_edge'] >= probaMin) &(ou['x_edge'] < probaMax) & (ou['prokuratura']==v['nazwa'])]).copy()
                 ext ['kat'] = g['kat']
-                if not ext.empy:
+                if not ext.empty:
                     if label in extracts[direction]:
                         extracts[direction][label] = pd.concat (
                             [extracts[direction][label], ext],
@@ -116,7 +114,10 @@ for v in out:
                         extracts[direction][label] = ext
             probaMax = probaMin
             if label in extracts[direction]:
-                extracts[direction][label].to_latex (
+                formatters = {c: euro_fmt for c in colsToKeep
+                              if pd.api.types.is_float_dtype(
+                                      extracts[direction][label][c])}
+                t = extracts[direction][label].to_latex (
                     columns=colsToKeep,
                     index=False,
                     longtable=True,
@@ -124,15 +125,25 @@ for v in out:
                     caption=f"Obwody z wynikiem  {EXPLAIN[label]} na korzyść {BENEF[direction]} według modelu" ,
                     label=f"tab:{direction}{label}",
                     
-                    column_format = "llllllllllllllllll"
+                    column_format = "llllllllllllllllll",
                     multicolumn=False
                     
                 )
+                with open(s / v['dir'] / 'tables.tex', "w", encoding="utf-8") as f:
+                    f.write(t)
 out.to_csv ('merge.csv', sep=';', index=True,
          header=True, encoding="utf-8",
          quoting=1, quotechar='"')
-         
 
+with open('doniesienie-mono.tex', encoding="utf-8") as f:
+    texSrc = f.read()
+
+for i, v in out.iterrows():
+    res = texSrc
+    for col in out.columns:
+        res = res.replace (f"===={col}====", v[col])
+    with open (s / v['dir'] / 'doniesienie.tex', "w", encoding="utf-8") as f:
+        f.write(res)
     
 # Teryt Gminy; Gmina; Powiat; Nr komisji; Siedziba; Uprawnienie / Liczba wyborców uprawnionych do głosowania (umieszczonych w spisie, z uwzględnieniem dodatkowych formularzy) w chwili zakończenia głosowania;
 # zaswiadczenia2t; w tym z powodu postawienia znaku „X” obok nazwisk obu kandydatów;
